4785f401a29e0b2db3886bcc1623969b74013fe7
Alok Bind <114241460+alokbind01@users.noreply.github.com>
2026-09-22 17:34:59 +0530

    feat: add CORS middleware for handling cross-origin requests

--- app/main.py
+++ app/main.py
@@ -2,6 +2,7 @@ from fastapi import FastAPI
 from app.database import Base, engine
 from app import models
 from app.routers import tickets
+from fastapi.middleware.cors import CORSMiddleware
 
 Base.metadata.create_all(bind=engine)
 
@@ -9,6 +10,14 @@ app = FastAPI(
     title="Support CRM API",
     description="Customer Support Ticketing CRM API",
     version="1.0.0",
+)
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["http://localhost:5173"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
 )
 
 app.include_router(tickets.router)
